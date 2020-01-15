a7fffcac3968de473860fa945d1f48e6d9f1cbc9
unknown <raudhiatul@gmail.com>
2020-01-15 16:01:57 +0700

    New

--- Prime.java
+++ Prime.java
@@ -1,6 +1,6 @@
 public class Prime {
     public static void main(String[] args) {
-        int num = 29;
+        int num = 7;
         boolean flag = false;
         for(int i = 2; i <= num/2; ++i)
         {
